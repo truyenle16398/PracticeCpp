3472e712985d33326bdf28ce7204e1644baa19fe
truyenle16398 <truyenle16398@gmail.com>
2019-11-20 11:49:40 +0700

    + Add Input Func

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -1,48 +1,76 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
-char arr[3][3] = {{'1', '2', '3'},{'4', '5', '6'},{'7', '8', '9'}};
+string arr[3][3] = {{"11", "12", "13"},{"21", "22", "23"},{"31", "32", "33"}};
+
 void board();
 int checkWin();
+void Input(string,string);
+
+string turn = "X";
+int row,col,choose;
+string player1Name,player2Name;
+
 
 int main(){
-	string player1Name,player2Name;
 	char h;
-	int choose;
-	
 	//Welcome Message
 	cout << "Welcome To Tic-tac-toe game! Play with your way! \nIf you find any problem, please contact john.nguyen@gameloft.com \n\n";
     cout << "SELECT YOUR MODE (1 - PLAY GAME | 2 - OTHER TO EXIT GAME): ";
-    //Choose Mode
     
+	//Choose Mode
     cin >> h;
     if (h != '1')
     {
 		cout << "--EXIT GAME--";
         system("exit");
     }
+    
     //Enter Player Name
     cout<<"\nEnter Player 1 Name: ";cin>>player1Name;
 	cout<<"Enter Player 2 Name: ";cin>>player2Name;
+	
+	while(checkWin() == 1){
+		board();
+		Input(player1Name,player2Name);
+		checkWin();
+	}
+	
+	system("cls");
 	board();
-	
-	cout<<"\nYour Turn "<<player1Name<<" - 0:";cin>>choose;
-	
-	
     return 0;
 }
 
+void Input(string player1Name, string player2Name){
+	if (turn == "X"){
+		cout<<"\nYour Turn "<<player1Name<<" - X: ";
+	}
+	else cout<<"\nYour Turn "<<player2Name<<" - 0: ";
+	cin>>choose;
+	row = choose/10;
+	col = choose%10;
+		
+	if(turn == "X" && arr[row][col] != "X" && arr[row][col] != "0"){
+		arr[row][col] == "X";
+		turn = "0";
+	}else if (turn == "0" && arr[row][col] != "X" && arr[row][col] != "0"){
+		arr[row][col] == "0";
+		turn = "0";
+	}else cout<<"Can't mark here";
+	Input(player1Name,player2Name);
+	}
 //Draw Board
 void board(){
 	cout << "\n\n TIC TAC TOE \n\n";
-	cout << "     |     |     " << endl;
-	cout << "  " << arr[0][0] << "  |  " << arr[0][1] << "  |  " << arr[0][2] << endl;
-	cout << "_____|_____|_____" << endl;
-    cout << "     |     |     " << endl;
+	cout << "      |      |     " << endl;
+	cout << "  " << arr[0][0]<< "  |  " << arr[0][1] << "  |  " << arr[0][2] << endl;
+	cout << "______|______|______" << endl;
+    cout << "      |      |     " << endl;
     cout << "  " << arr[1][0] << "  |  " << arr[1][1] << "  |  " << arr[1][2] << endl;
-    cout << "_____|_____|_____" << endl;
-    cout << "     |     |     " << endl;
+    cout << "______|______|______" << endl;
+    cout << "      |      |     " << endl;
     cout << "  " << arr[2][0] << "  |  " << arr[2][1] << "  |  " << arr[2][2] << endl;
 }
 
