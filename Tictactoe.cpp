3f303a47ca1101f52e9db359ada9fde17492fafc
truyenle16398 <truyenle16398@gmail.com>
2019-11-20 09:44:28 +0700

    + Add Enter Player Name | Draw Board

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+char arr[3][3] = {{'1', '2', '3'},{'4', '5', '6'},{'7', '8', '9'}};
+void board();
+
 int main(){
 	 cout << "Welcome To Tic-tac-toe game! Play with your way! \nIf you find any problem, please contact john.nguyen@gameloft.com";
     cout << "SELECT YOUR MODE (1 - PLAY GAME | 2 - OTHER TO EXIT GAME)\n";
@@ -12,5 +15,20 @@ int main(){
 		cout << "EXIT GAME";
         system("exit");
     }
+    string player1Name,player2Name;
+    cout<<"Enter Player 1 Name: ";cin>>player1Name;
+	cout<<"Enter Player 2 Name: ";cin>>player2Name;
+	board();
     return 0;
+}
+void board(){
+	cout << "\n\n TIC TAC TOE \n\n";
+	cout << "     |     |     " << endl;
+	cout << "  " << arr[0][0] << "  |  " << arr[0][1] << "  |  " << arr[0][2] << endl;
+	cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << arr[1][0] << "  |  " << arr[1][1] << "  |  " << arr[1][2] << endl;
+    cout << "_____|_____|_____" << endl;
+    cout << "     |     |     " << endl;
+    cout << "  " << arr[2][0] << "  |  " << arr[2][1] << "  |  " << arr[2][2] << endl;
 }
