a307967b7ac38e162cfa3d9a4e4c46114c60c815
truyenle16398 <truyenle16398@gmail.com>
2019-11-21 15:04:23 +0700

    + Add file Ex3.cpp

---
+++ Pointer_STL/Ex3.cpp
@@ -0,0 +1,14 @@
+// Tra lai bo nho ko dung den cho may, tranh lang phi bo nho
+// Sau khi dung xong con tro trong truong hop con tro co cap phat bo nho
+//delete dung de giai phong bo nho cho 1 bien, delete[] dung de giai phong con tro cho 1 mang
+
+//DEMO: 
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+		
+	return 0;
+}	
