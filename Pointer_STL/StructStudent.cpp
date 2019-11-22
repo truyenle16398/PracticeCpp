71d28a9b433ac7ab2516a45efbe13ca347e88f0a
truyenle16398 <truyenle16398@gmail.com>
2019-11-22 11:48:58 +0700

    * Fix Display Func

--- Pointer_STL/StructStudent.cpp
+++ Pointer_STL/StructStudent.cpp
@@ -17,13 +17,14 @@ void Display(vector<Student*>);
 vector<Student*> listStudent;
 
 
-int main(){
-	Student* student;
+int main() {
+	Student* student = new Student;
 	int choose;
 	DrawMenu();
-	do{
-		cout<<"Choose: ";cin>>choose;
-		switch (choose){
+
+	do {
+		cout << "Choose: "; cin >> choose;
+		switch (choose) {
 		case 1: {
 			Input();
 			break;
@@ -32,67 +33,68 @@ int main(){
 			Display(listStudent);
 			break;
 		}
-		case 0:{
-			cout<<"Exit.....\n";
+		case 0: {
+			cout << "Exit.....\n";
 			exit(0);
 			break;
 		}
-	}
-	}while(choose!=0);
-	
+		}
+	} while (choose != 0);
+
 	return 0;
 }
 
-void Input(){
+void Input() {
 	int n;
-	cout<<"Enter number of student: ";cin>>n;
-	
-	if(n > 0){
-		for(int i = 0; i < n; i++){
+	cout << "Enter number of student: "; cin >> n;
+
+	if (n > 0) {
+		for (int i = 0; i < n; i++) {
 			Student *student = new Student;
-			cout<<"\n---Student "<<i+1<<"--- "<<endl<<endl;
-			
-			cout<<"Enter ID student: "; cin>>student->id;
-			
-			cout<<"Enter Fullname student: ";cin>>student->name;
-			
+			cout << "\n---Student " << i + 1 << "--- " << endl << endl;
+
+			cout << "Enter ID student: "; cin >> student->id;
+
+			cout << "Enter Fullname student: "; cin >> student->name;
+
 			do
 			{
-				cout<<"Enter score: ";cin>>student->score;
-				if(student->score < 0 || student->score > 10){
-				cout<<"Invalid score. \nEnter again.\n";
-			}
-			}
-			while (student->score < 0 || student->score > 10);
+				cout << "Enter score: "; cin >> student->score;
+				if (student->score < 0 || student->score > 10) {
+					cout << "Invalid score. \nEnter again.\n";
+				}
+			} while (student->score < 0 || student->score > 10);
 			listStudent.push_back(student);
+		}
 	}
-	}else {
-		cout<<"Invalid number";
+	else {
+		cout << "Invalid number";
 		Input();
 	}
-	
+
 }
 
-void Display(vector<Student*> totalStudent){
-	
-	cout << "ID\tFULL NAME\tSCORE"<<endl;
+void Display(vector<Student*> list) {
+
+	cout << "ID\tFULL NAME\tSCORE" << endl;
 	cout << "___________________________________________\n";
-	
-	for (int i = 0; i <= totalStudent.size(); i++)
-	{
-		cout << totalStudent[i]->id << "\t" << totalStudent[i]->name << "\t\t" << totalStudent[i]->score << endl;
+
+	for (size_t i = 0; i < list.size(); i++) {
+		cout << list[i]->id << "\t" << list[i]->name << "\t" << list[i]->score << endl;
+
 	}
 }
 
-void DrawMenu(){
-	cout<<"------------------MENU------------------"<<endl;
-	cout<<"1. Input"<<endl;
-	cout<<"2. Display"<<endl;
-	cout<<"3. Save to file"<<endl;
-	cout<<"4. Load from file"<<endl;
-	cout<<"0. Exit"<<endl;
-	cout<<"----------------------------------------"<<endl;
+void DrawMenu() {
+	cout << "------------------MENU------------------" << endl;
+	cout << "1. Input" << endl;
+	cout << "2. Display" << endl;
+	cout << "3. Save to file" << endl;
+	cout << "4. Load from file" << endl;
+	cout << "0. Exit" << endl;
+	cout << "----------------------------------------" << endl;
 }
+
 
 
 
