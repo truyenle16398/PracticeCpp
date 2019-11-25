127599fff1095edbaa14d254e2c4c9d28a644e58
truyenle16398 <truyenle16398@gmail.com>
2019-11-25 09:29:31 +0700

    + Add WriteVirus

---
+++ OOP/WriteVirus/WriteVirus/Virus.cpp
@@ -0,0 +1,56 @@
+﻿#include "stdafx.h"
+#include "Virus.h"
+#include<iostream>
+#include <fstream>
+#include <string>
+
+
+using namespace std;
+
+
+Virus::Virus()
+{
+}
+
+Virus::Virus(char * m_dna1, int m_res)
+{
+	m_dna = m_dna1;
+	m_resistance = m_res;
+}
+
+
+Virus::Virus(Virus & v)
+{
+	m_dna = v.m_dna;
+	m_resistance = v.m_resistance;
+}
+
+Virus::~Virus()
+{
+}
+
+int Virus::LoadADNInformation​()
+{
+	ifstream fi;
+	fi.open("ATGX.bin", ios::in);
+	if (fi.fail())
+	{
+		cout << "Can't load from file" << endl;
+		return -1;
+	}
+	string dna;
+	getline(fi, dna);
+	for (int i = 0; i < dna.length(); i++)
+	{
+		this->m_dna[i] = dna[i];
+	}
+	fi.close();
+	return 0;
+
+}
+
+void Virus::ReduceResistance(int medicine_resistance)
+{
+	m_resistance -= medicine_resistance;
+}
+
---
+++ OOP/WriteVirus/WriteVirus/Virus.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+class Virus
+{
+private: 
+	char* m_dna;
+	int m_resistance;
+public:
+	Virus();
+	Virus(char *m_dna1, int m_res);
+	//Copy contrustor
+	Virus(Virus &v);
+	~Virus();
+	int LoadADNInformation​();
+	void ReduceResistance(int medicine_resistance);
+	virtual void DoBorn();
+};
+
